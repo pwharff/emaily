976d066a5fce089a11c2e29aeb022c4dc8e39239
Wharff <pwharff@SCUNBCHQ12596.local>
2018-04-13 08:42:54 -0700

    Passport and MongoDB support

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport')
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: "there",
-    bye: "buddy"
-  });
-});
+mongoose.connect(keys.mongoURI)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const app = express()
+require('./routes/authRoutes')(app)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
